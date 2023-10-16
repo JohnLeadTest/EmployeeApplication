35f2ee15b1219478e935a763efc56cdc299bc78b
john morris <fennymorris@live.co.uk>
2023-10-16 15:23:33 +0100

    better maping for request for readability

--- src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
+++ src/main/java/com/employee/EmployeeApplication/controller/EmployeeController.java
@@ -17,27 +17,32 @@ public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
-    @RequestMapping("/employees")
+
+//    @RequestMapping("/employees") # instead of this we can use getmapping
+    @GetMapping("/employees")
     public List<Employee> findAllEmployees() {
         return  employeeService.getAllEmployees();
     }
 
-    @RequestMapping("/employees/{id}")
+    @GetMapping("/employees/{id}")
     public Employee findAnEmployees(@PathVariable int id) {
         return  employeeService.getAnEmployee(id);
     }
 
-    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+//    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+    @PostMapping("/employees")
     public  void createEmployee(@RequestBody Employee employee) {
         employeeService.createEmployee(employee);
     }
 
-    @RequestMapping(value = "employees/{id}", method = RequestMethod.PUT)
+    @PutMapping("/employees/{id}")
+//    @RequestMapping(value = "employees/{id}", method = RequestMethod.PUT)
     public void updateEmployee(@PathVariable  int id, @RequestBody Employee employee) {
         employeeService.updateEmployee((employee));
     }
 
-    @RequestMapping(value = "employees/{id}", method = RequestMethod.DELETE)
+//    @RequestMapping(value = "employees/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("employees/{id}")
     public List<Employee> deleteEmployee(@PathVariable int id) {
         employeeService.deleteEmployee(id);
         return employeeService.getAllEmployees();
